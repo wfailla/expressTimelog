26e1f771859cd246d1c166a70f02f5af4c374eea
Willy Failla <wfailla@wfailla.de>
2021-05-27 10:38:12 +0200

    zwischenstand

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -1,6 +1,5 @@
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-import path from 'path';
 import helmet from 'helmet';
 
 import express, { NextFunction, Request, Response } from 'express';
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,25 @@
 import './pre-start'; // Must be the first import
 import app from '@server';
 import logger from '@shared/Logger';
+import { createConnection } from 'typeorm';
+import dbConfig from './config/db';
 
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
-});
+// app.listen(port, () => {
+//     logger.info('Express server started on port: ' + port);
+// });
+
+
+// init db connection
+createConnection(dbConfig)
+  .then((_connection) => {
+    app.listen(port, () => {
+      console.log("Server is running on port", port);
+    });
+  })
+  .catch((err) => {
+    console.log("Unable to connect to db", err);
+    process.exit(1);
+  });
