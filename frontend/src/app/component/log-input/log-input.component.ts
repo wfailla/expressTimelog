07aa7c56916d994467f592618bd0ca60402f5c23
Willy Failla <wfailla@wfailla.de>
2021-05-28 11:47:57 +0200

    add passed time display

--- frontend/src/app/component/log-input/log-input.component.ts
+++ frontend/src/app/component/log-input/log-input.component.ts
@@ -3,6 +3,8 @@ import { FormControl } from '@angular/forms';
 import { LogEntity } from 'src/app/model/log-entity';
 import { BackendService } from 'src/app/service/backend.service';
 import { DatePipe } from '@angular/common';
+import { interval } from 'rxjs';
+import { Timestamp } from 'src/app/model/timestamp';
 
 @Component({
   selector: 'app-log-input',
@@ -11,6 +13,10 @@ import { DatePipe } from '@angular/common';
 })
 export class LogInputComponent implements OnInit {
   activityFormControl = new FormControl('', []);
+  timestamp: string = "";
+  currentDiff: string = "";
+  day: Date = new Date(Date.now());
+  lastTimestamp: Timestamp = new Timestamp(new Date());;
 
   constructor(
     private backend: BackendService,
@@ -18,6 +24,17 @@ export class LogInputComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.backend.getLogEntries(this.datepipe.transform(this.day, 'yyyy-MM-dd') || '').subscribe(
+      s => {
+        this.lastTimestamp = new Timestamp(new Date(s[s.length - 1].timestamp));
+        this.timestamp = this.datepipe.transform(this.lastTimestamp.date, 'yyyy-MM-dd HH:mm') || '';
+        this.calcActivityTime();
+      }
+    )
+    interval(60 * 1000)
+      .subscribe(() => {
+        this.calcActivityTime();
+      })
   }
 
   addActivity(): void {
@@ -27,8 +44,8 @@ export class LogInputComponent implements OnInit {
     };
 
     var date = new Date();
-    var timestamp = this.datepipe.transform(date, 'yyyy-MM-dd HH:mm');
-    logEntry.timestamp = timestamp || '';
+    this.timestamp = this.datepipe.transform(date, 'yyyy-MM-dd HH:mm') || '';
+    logEntry.timestamp = this.timestamp || '';
 
     if (this.activityFormControl?.value !== undefined && this.activityFormControl?.value !== "") {
       logEntry.activity = this.activityFormControl.value;
@@ -39,6 +56,13 @@ export class LogInputComponent implements OnInit {
         this.backend.changeDone();
       }
     )
+    this.calcActivityTime();
+  }
+
+  calcActivityTime(): void {
+    var currentTimestamp = new Timestamp(new Date());
+    this.lastTimestamp = new Timestamp(new Date(this.timestamp));
+    this.currentDiff = this.lastTimestamp.diff(currentTimestamp);
   }
 
 }
