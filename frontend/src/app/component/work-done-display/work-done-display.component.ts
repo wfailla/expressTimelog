c78e10a545329d85aa80bc3c5e935121eb626f45
Willy Failla <wfailla@wfailla.de>
2021-06-20 11:50:09 +0200

    display work done for selected day

--- frontend/src/app/component/work-done-display/work-done-display.component.ts
+++ frontend/src/app/component/work-done-display/work-done-display.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { LogEntity } from 'src/app/model/log-entity';
 import { Timestamp, TimeString } from 'src/app/model/timestamp';
 import { BackendService } from 'src/app/service/backend.service';
+import { DateService } from 'src/app/service/date.service';
 
 @Component({
   selector: 'app-work-done-display',
@@ -11,14 +12,16 @@ import { BackendService } from 'src/app/service/backend.service';
 })
 export class WorkDoneDisplayComponent implements OnInit {
   workDone: string = '';
+  day: Date = new Date(Date.now());
 
   constructor(
     private backend: BackendService,
     private datepipe: DatePipe,
+    private dateService: DateService,
   ) { }
 
   ngOnInit(): void {
-    this.backend.getLogEntries(this.datepipe.transform(new Date(), 'yyyy-MM-dd') || '').subscribe(
+    this.backend.getLogEntries(this.datepipe.transform(this.day, 'yyyy-MM-dd') || '').subscribe(
       s => {
         this.calcDiff(s);
       }
@@ -26,11 +29,16 @@ export class WorkDoneDisplayComponent implements OnInit {
 
     this.backend.changeOperation.subscribe(
       s => {
-        this.backend.getLogEntries(this.datepipe.transform(new Date(), 'yyyy-MM-dd') || '').subscribe(
+        this.backend.getLogEntries(this.datepipe.transform(this.day, 'yyyy-MM-dd') || '').subscribe(
           o => {
             this.calcDiff(o);
           }
         )
+      }
+    )
+    this.dateService.date.subscribe(
+      s => {
+        this.day = s;
       }
     )
   }
