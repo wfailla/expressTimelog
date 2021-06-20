0531ce1b155beee56f1482bd97be00c5fbf1b33a
Willy Failla <wfailla@wfailla.de>
2021-06-20 12:05:27 +0200

    display slacking time for selected day

--- frontend/src/app/component/slacking-time-display/slacking-time-display.component.ts
+++ frontend/src/app/component/slacking-time-display/slacking-time-display.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { LogEntity } from 'src/app/model/log-entity';
 import { Timestamp } from 'src/app/model/timestamp';
 import { BackendService } from 'src/app/service/backend.service';
+import { DateService } from 'src/app/service/date.service';
 
 @Component({
   selector: 'app-slacking-time-display',
@@ -11,31 +12,35 @@ import { BackendService } from 'src/app/service/backend.service';
 })
 export class SlackingTimeDisplayComponent implements OnInit {
   slackingTime: string = '';
+  day: Date = new Date(Date.now());
 
   constructor(
     private backend: BackendService,
     private datepipe: DatePipe,
+    private dateService: DateService,
   ) { }
 
   ngOnInit(): void {
-    this.backend.getLogEntries(this.datepipe.transform(new Date(), 'yyyy-MM-dd') || '').subscribe(
-      s => {
-        this.calcDiff(s);
-      }
-    )
+    this.calcDiff();
 
     this.backend.changeOperation.subscribe(
       s => {
-        this.backend.getLogEntries(this.datepipe.transform(new Date(), 'yyyy-MM-dd') || '').subscribe(
-          o => {
-            this.calcDiff(o);
-          }
-        )
+        this.calcDiff();
+      }
+    )
+    this.dateService.date.subscribe(
+      s => {
+        this.day = s;
+        this.calcDiff();
       }
     )
   }
 
-  calcDiff(list: LogEntity[]): void {
-    this.slackingTime = Timestamp.calculateSlackingTime(list);
-  }
+  calcDiff(): void {
+      this.backend.getLogEntries(this.datepipe.transform(this.day, 'yyyy-MM-dd') || '').subscribe(
+        o => {
+          this.slackingTime = Timestamp.calculateSlackingTime(o);
+        }
+      )
+    }
 }
